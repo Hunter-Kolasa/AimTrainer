d7671fa38ac2edfb559d21fe3bca930137dc5da3
Hunter <kolasa.hunter@aol.com>
2021-03-10 12:31:02 -0500

    click-to-erase works, targets spawn correctly, console.log hits&misses

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,22 +1,17 @@
-
 const canvas = document.getElementById("window");
 const c = canvas.getContext("2d");
 const targets = [];
+const clicks = [];
+let score = 0
+let missCounter = 0
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-document.addEventListener("DOMContentLoaded", function() {
-    startRound();
-    window.addEventListener('click', (mc) => {
-        targetHit(mc.clientX, mc.clientY);
-    })
-})
-
 class Target {
-    constructor() {
-        this.radius = Math.random() * (30 - 2) + 2
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
+    constructor(x, y, radius) {
+        this.radius = radius
+        this.x = x
+        this.y = y
         this.color = getRandomColor()  
     }
 
@@ -33,6 +28,32 @@ class Target {
         this.draw()
     }
 }
+
+// class Click {
+//     constructor(x, y) {
+//         this.radius = 2
+//         this.x = x - 8
+//         this.y = y - 8
+//         this.color = "#000000"  
+//     }
+
+//     draw() {
+//         c.beginPath()
+//         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+//         c.closePath
+//         c.fillStyle = this.color
+//         c.fill()
+        
+//     }
+
+//     update() {
+//         this.draw()
+//     }
+// }
+
+document.addEventListener("DOMContentLoaded", function() {
+    startRound();
+})
 
 // const myGameArea = {
 //     canvas: document.createElement("canvas"),
@@ -54,7 +75,11 @@ class Target {
 // }
 
 function startRound() {
-    setInterval(addTarget, 1000)
+    window.addEventListener('mousedown', (mc) => {
+        // clicks.push(new Click(mc.clientX, mc.clientY))
+        targetHit(mc.clientX, mc.clientY);
+    })
+    setInterval(addTarget, 500)
 }
 
 function getRandomColor() {
@@ -67,27 +92,47 @@ function getRandomColor() {
 }
 
 function addTarget() {
-    targets.push(new Target())
-    console.log(targets)
+    const radius = Math.random() * (30 - 12) + 12
+    let x = Math.random() * (canvas.width)
+    let y = Math.random() * (canvas.height)
+    while(x < (canvas.width / 10) || x > 9 * (canvas.width / 10)) {
+        x = Math.random() * (canvas.width)
+    }
+    while(y < (canvas.height / 8) || y > 7 * (canvas.height / 8)) {
+        y = Math.random() * (canvas.height)
+    }
+    targets.push(new Target(x, y, radius))
 }
 
 function targetHit(x, y) {
+    priorScore = score
     targets.forEach(t => {
         if (Math.sqrt((x-t.x)*(x-t.x) + (y-t.y)*(y-t.y)) < t.radius) {
             targets.splice(targets.indexOf(t), 1)
+            score++
+            console.log("Score: " + score)
+            hitSpray(t)
         }
-        
     })
+    if (priorScore === score) {
+        console.log("Misses: " + ++missCounter)
+    }
+}
+
+function hitSpray(targetHit) {
+
 }
 
 function insideTarget(mX, mY, tX, tY, tR) {
-    Math.sqrt((mX-tX)*(mX-tX) + (mY-tY)*(mY-tY)) < tR ? true : false
+    Math.sqrt((mX-tX)*(mX-tX) + (mY-tY)*(mY-tY)) <= tR ? true : false
 }
-
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
+    // clicks.forEach((click) => {
+    //     click.update();
+    // })
     targets.forEach((target) => {
         target.update();
     })
