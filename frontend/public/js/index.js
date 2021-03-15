148f2d2dcb7f1fcdb17a065c3355788ae78d0642
Hunter <kolasa.hunter@aol.com>
2021-03-15 17:26:00 -0400

    Added endgame conditional, much hidden/unhidden debuggin needed

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,18 +1,53 @@
 const BASE_URL = "http://localhost:3000/";
-const canvasDiv = document.querySelector(".canvas")
-const canvas = document.createElement("canvas");
-canvas.hidden = true
-canvas.height = innerHeight;
-canvas.width = innerWidth;
-canvasDiv.appendChild(canvas)
-const c = canvas.getContext("2d");
-const errorDiv = document.querySelector(".alert");
-const userForm = document.forms["login-signup"];
-const loginBtn = document.getElementById("loginBtn");
+// canvas build
+    const canvasDiv = document.querySelector(".canvas")
+    const canvas = document.createElement("canvas");
+    canvas.hidden = true
+    canvas.height = innerHeight;
+    canvas.width = innerWidth;
+    canvasDiv.appendChild(canvas)
+    const c = canvas.getContext("2d");
+// 
+// scorecard w/ space for username
+    const scoreDiv = document.querySelector(".scorecard");
+    const scorecardH3 = document.createElement("h3");
+    const usernameH4 = document.createElement("h4");
+    scorecardH3.hidden = true
+    usernameH4.hidden = true
+    scoreDiv.appendChild(usernameH4);
+    scoreDiv.appendChild(scorecardH3);
+// 
+// error logging div
+    const errorDiv = document.querySelector(".alert");
+// 
+// user login form/btn
+    const userForm = document.forms["login-signup"];
+    const loginBtn = document.getElementById("loginBtn");
+// start btn create
+    const startBtnDiv = document.querySelector(".start-game-btn")
+    const startBtn = document.createElement("button")
+    startBtn.hidden = true;
+// 
+// end menu with replay/main menu buttons and
+// either loss message or final score
+    const endMenuDiv = document.querySelector(".end-menu");
+    const replayBtn = document.createElement("button");
+    replayBtn.hidden = true;
+    replayBtn.innerText = "Play Again"
+    const mainMenuBtn = document.createElement("button");
+    mainMenuBtn.hidden = true;
+    mainMenuBtn.innerText = "Main Menu"
+    const endMessageP = document.createElement("p");
+    endMessageP.hidden = true;
+    endMenuDiv.appendChild(endMessageP);
+    endMenuDiv.appendChild(replayBtn);
+    endMenuDiv.appendChild(mainMenuBtn);
+// 
 const targets = [];
 let score = 0;
 let missCounter = 0;
 let stop = 0
+
 // const clicks = [];
 
 class Target {
@@ -100,7 +135,8 @@ function userLoginOrSignup(user) {
             return response.json()
         } )
         .then(function(object) {
-            userMenu(object)
+            userForm.hidden = true
+            userMenu(object.username)
         } )
         .catch(function(error) {
             alert("Something went wrong.")
@@ -108,15 +144,19 @@ function userLoginOrSignup(user) {
         } )
 }
 
-function userMenu(user) {
-    const startBtnDiv = document.querySelector(".start-game-btn")
-    const startBtn = document.createElement("button")
+function userMenu(username) {
+    scorecardH3.hidden = true
+    usernameH4.hidden = true
+    userForm.hidden = true
     startBtn.setAttribute('class', 'startBtn')
     startBtn.innerText = "Start Round"
     startBtnDiv.appendChild(startBtn)
+    startBtn.hidden = false
     startBtn.addEventListener('click', () => {
-        userForm.hidden = true
         startBtn.hidden = true
+        usernameH4.innerHTML = username
+        
+
         startRound();
     })
     
@@ -132,6 +172,13 @@ function userMenu(user) {
 
 // listens for mousedown and starts target generating loop
 function startRound() {
+    score = 0
+    scorecardH3.hidden = false
+    scorecardH3.innerHTML = score
+    replayBtn.hidden = true;
+    mainMenuBtn.hidden = true;
+    endMessageP.hidden = true;
+    usernameH4.hidden = false
     canvas.hidden = false
     window.addEventListener('mousedown', (mc) => {
         // clicks.push(new Click(mc.clientX, mc.clientY))
@@ -152,6 +199,10 @@ function getRandomColor() {
 
 // generates random target params within limits and adds to targets array
 function addTargets() {
+    if (targets.length > 29) {
+            let state = "loss"
+            endGame(state)
+        }
     setInterval(() => {
         let radius = Math.random() * (30 - 12) + 12
     let x = Math.random() * (canvas.width)
@@ -172,16 +223,26 @@ function addTargets() {
 // if score doesn't go up, registers as miss, adds to missCounter
 function targetHit(x, y) {
     priorScore = score
+    let user = usernameH4.value
     targets.forEach(t => {
         if (Math.sqrt((x-t.x)*(x-t.x) + (y-t.y)*(y-t.y)) <= t.radius) {
             targets.splice(targets.indexOf(t), 1)
-            score++
-            console.log("Score: " + score)
+            scorecardH3.innerHTML = ++score
             hitSpray(t)
         }
     })
     if (priorScore === score) {
-        console.log("Misses: " + ++missCounter)
+        missCounter++
+        if (missCounter < 5){
+            if (score <= 0) {
+                score = 0
+                scorecardH3.innerHTML = score
+            } else {
+                scorecardH3.innerHTML = --score
+            }
+        } else {
+            submitScore(user, score);
+        }
     }
 };
 
@@ -190,16 +251,58 @@ function hitSpray(targetHit) {
     // insert cool animation for target hit here
 };
 
-function endGame() {
-    // displays end screen, calls submitScore() and gives option to start again or return to main menu
+function submitScore(user, score) {
+    console.log(`Game submitting with username: ${user} and score: ${score}.`)
+    return fetch(BASE_URL + "games", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            username: user,
+            game: score
+        } )
+    } )
+        .then(function(response) {
+            return response.json()
+        } )
+        .then(function(object) {
+            let state = "win"
+            endGame(state, object)
+        } )
+        .catch(function(error) {
+            alert("Something went wrong while submitting score")
+            console.log(error.message)
+        } )
 }
 
-function submitScore() {
-    // sends fetch() post back to API
+function endGame(state, score) {
+    // displays end screen, calls submitScore() and gives option to start again or return to main menu
+    canvas.hidden = true;
+    replayBtn.hidden = false;
+    mainMenuBtn.hidden = false;
+    endMessageP.hidden = false;
+    if (state === "loss" ) {
+        endMessageP.innerHTML = "You couldn't keep up!"
+    }
+    if (state === "win" ) {
+        endMessageP.innerHTML = `Congratulations! You finished with ${score} points!`
+    }
+    replayBtn.addEventListener("click", function() {
+        startRound();
+    })
+    mainMenuBtn.addEventListener("click", function() {
+        userMenu(usernameH4.value);
+    })
+
 }
+
+
 
 // frame request loop that updates targets every frame
-function animate(stop = 0) {
+function animate() {
+    // while (stop === 0) {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     // clicks.forEach((click) => {
@@ -208,10 +311,7 @@ function animate(stop = 0) {
     targets.forEach((target) => {
         target.update();
     })
-    if (targets.length > 30) {
-
-    }
-    //***** */ if stop = 0 *****
+    
 };
 
 animate();
